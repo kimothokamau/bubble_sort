0bc13770af6e57b078386c2973968db7009d3660
kimothokamau <kimothokamau@gmail.com>
2020-12-24 08:08:04 -0600

    implement bubble sort and bubble sortby

---
+++ bubble_sort.rb
@@ -0,0 +1,37 @@
+array = [5, 6, 3, 7, 3, 1, 9, 1]
+def bubble_sort(array)
+  n = array.length - 1
+  swapped = true
+  while swapped
+    swapped = false
+    n.times do |i|
+      if array[i] > array[i + 1]
+        array[i], array[i + 1] = array[i + 1], array[i]
+        swapped = true
+      end
+    end
+  end
+  puts array
+end
+bubble_sort(array)
+
+def bubble_sort_by(arr)
+  puts arr if arr.length <= 1
+  n = arr.length - 1
+
+  swapped = true
+  while swapped
+    swapped = false
+    n.times do |i|
+      if yield(arr[i], arr[i + 1]).positive?
+        arr[i], arr[i + 1] = arr[i + 1], arr[i]
+        swapped = true
+      end
+    end
+  end
+  puts arr
+end
+
+bubble_sort_by(%w[hi hello hey]) do |left, right|
+  left.length - right.length
+end
